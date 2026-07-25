30c4f094fb959daaff0fdf518ac0790966b26515
Restenpoche <restenpochebillard@yahoo.com>
2026-07-25 19:29:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,39 @@ section.style.transform = "translateY(40px)";
 section.style.transition = "all 0.8s ease";
 observer.observe(section);
 });
+// ==========================
+// MENU MOBILE COULISSANT
+// ==========================
+
+const menu = document.getElementById("menu");
+const bouton = document.getElementById("menu-toggle");
+
+// Création du fond sombre
+const overlay = document.createElement("div");
+overlay.id = "overlay";
+document.body.appendChild(overlay);
+
+overlay.style.cssText = position:fixed; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,.5); display:none; z-index:1500;;
+
+function fermerMenu() {
+menu.classList.remove("active");
+bouton.innerHTML = "☰";
+overlay.style.display = "none";
+}
+
+bouton.addEventListener("click", () => {
+menu.classList.toggle("active");
+
+if (menu.classList.contains("active")) {
+bouton.innerHTML = "✕";
+overlay.style.display = "block";
+} else {
+fermerMenu();
+}
+});
+
+overlay.addEventListener("click", fermerMenu);
+
+document.querySelectorAll("#menu a").forEach(lien => {
+lien.addEventListener("click", fermerMenu);
+});
